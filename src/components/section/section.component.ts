22fe74660b390952665a11a205edf2e8b4251e6f
Clément Contet <clement.contet@orange.com>
2017-06-07 15:36:19 +0200

    Remove paging + load 20 pics

--- src/components/section/section.component.ts
+++ src/components/section/section.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Http } from '@angular/http';
-import { NavParams, Slides } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
 import { PictureModel } from '../../models/picture.model';
@@ -10,7 +10,6 @@ import { PictureModel } from '../../models/picture.model';
     templateUrl: 'section.component.html'
 })
 export class Section {
-    @ViewChild(Slides) slides: Slides;
     tag: String;
     pictures: Array<PictureModel> = [];
     pendingQuery = false;
@@ -20,24 +19,8 @@ export class Section {
     }
 
     ngOnInit() {
-        this.http.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" + "purple" + "&sort=interestingness-desc&api_key=0c731f4470260b5ff4ccc3d519d07697&format=json&nojsoncallback=1&per_page=10")
+        this.http.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" + "purple" + "&sort=interestingness-desc&api_key=0c731f4470260b5ff4ccc3d519d07697&format=json&nojsoncallback=1&per_page=20")
             .map(response => response.json().photos.photo)
             .subscribe(pictures => this.pictures = pictures);
-    }
-
-    slideChanged() {
-        let currentIndex = this.slides.getActiveIndex();
-        if (currentIndex > this.pictures.length - 5 && !this.pendingQuery) {
-            let newPage = this.pictures.length / 10 + 1;
-
-            this.pendingQuery = true;
-            this.http.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=" + "purple" + "&sort=interestingness-desc&api_key=0c731f4470260b5ff4ccc3d519d07697&format=json&nojsoncallback=1&per_page=10&page=" + newPage)
-                .map(response => response.json().photos.photo)
-                .subscribe(pictures => {
-                    this.pictures = this.pictures.concat(pictures);
-                    this.slides.update();
-                    this.pendingQuery = false;
-                });
-        }
     }
 }
